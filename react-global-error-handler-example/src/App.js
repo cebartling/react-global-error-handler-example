8f501ed178848e99a29a8a801930f41a56b15f79
Christopher Bartling <chris.bartling@gmail.com>
2017-02-18 15:11:32 -0600

    Checkpoint.

--- react-global-error-handler-example/src/App.js
+++ react-global-error-handler-example/src/App.js
@@ -1,21 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import {Navbar, Nav, NavItem, Jumbotron, Button} from "react-bootstrap";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+
+    render() {
+        return (
+            <div className="App">
+                <Navbar>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <a href="#">React Global Error Handler Example</a>
+                        </Navbar.Brand>
+                    </Navbar.Header>
+                    <Nav>
+                        <NavItem eventKey={1} href="#">Create error</NavItem>
+                    </Nav>
+                </Navbar>
+
+                <Jumbotron>
+                    <h1>Hello, world!</h1>
+                    <p>To get started, edit <code>src/App.js</code> and save to reload.</p>
+                    <p><Button bsStyle="primary">Learn more</Button></p>
+                </Jumbotron>
+            </div>
+        );
+    }
 }
 
 export default App;
